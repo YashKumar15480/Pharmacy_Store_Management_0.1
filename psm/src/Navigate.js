9c27e447ad0c79ef3b8264f54d06fd4680987fde
YASH KUMAR <yaku15480@gmail.com>
2021-11-19 23:28:35 +0530

    psm

--- psm/src/Navigate.js
+++ psm/src/Navigate.js
@@ -4,8 +4,34 @@ import React from 'react'
 const Navigate = () => {
     return (
         <div>
-           <h2>Hellow from navigation component</h2> 
-        </div>
+        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <div className="container-fluid">
+<Link className="navbar-brand" to="#">Placement</Link>
+<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+</button>
+
+<div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+  
+      <li className="nav-item">
+         <Link className="nav-link active" aria-current="page" to="#">Home</Link>
+     </li>
+      <li className="nav-item">
+         <Link className="nav-link" to="#">Contact</Link>
+     </li>
+      <li className="nav-item">
+         <Link className="nav-link" to="#">AboutUs</Link>
+     </li>
+    
+    </ul>
+</div>
+
+</div>
+</nav> */}
+<h1>Navigation console.error();</h1>
+    </div>
     )
 }
 
