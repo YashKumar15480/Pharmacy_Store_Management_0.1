0dbd2e58ba51095e40b0e1efe6444a7ef684cf23
YASH KUMAR <yaku15480@gmail.com>
2021-11-19 22:11:19 +0530

    psm

--- psm/src/App.js
+++ psm/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigate from './Navigate';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to Pharmacy Store Management </h1>
+     <Navigate></Navigate>
     </div>
   );
 }
---
+++ psm/src/Navigate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navigate() {
+    return (
+    <div>
+    
+    <h1>this is from navigation </h1>
+    
+    </div>
+    )
+}
+export default Navigate
