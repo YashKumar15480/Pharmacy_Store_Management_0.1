05aff817c5caba844e67be5d310d01a6d52c95f9
YASH KUMAR <yaku15480@gmail.com>
2021-11-20 13:01:54 +0530

    psm

--- psm/src/App.js
+++ psm/src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import Home from './Home';
 import Contact from './Contact';
 import Navigate from './Navigate';
-// import Heading from './Heading';
+import Heading from './Heading';
 import Yash from './Yash';
-// import { Route, Router } from 'react-router';
 import {BrowserRouter as Switch,Router, Route} from 'react-router-dom'
 import Placement from './Placement';
 
 function App() {
   return (
 
-
+      
       <Router>
        <div className ="App">
        <Navigate></Navigate>
+       <Heading></Heading>
        <Switch>
        <Route exact path = "/Home" component = {Home}></Route>
        <Route exact path = "/Yash" component = {Yash}></Route>
