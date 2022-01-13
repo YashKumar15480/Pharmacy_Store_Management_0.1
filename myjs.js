76e154cb00877aa9c22976847bb52e9656109945
YASH KUMAR <yaku15480@gmail.com>
2022-01-13 10:03:27 +0530

    psm

--- myjs.js
+++ myjs.js
@@ -7,3 +7,12 @@ function check(){
   if(fname == "riya" && lname == "1234") return true;
   else {alert("Invalid User");return false;}
 }  
+function check1()
+{
+
+  let email = document.forms["form1"]["email"].value
+
+  if(/[a-z,A-Z,0-9_\-\.]+[@][a-z]+[\.][a-z]{2,3}/.test(email)) return true;
+  else {alert("Invalid Email"); return false}
+
+}
